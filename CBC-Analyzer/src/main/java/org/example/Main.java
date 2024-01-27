5bbdfedd4d719c79a3a870303851144beb6a7c85
unknown <beaucblevins@gmail.com>
2024-01-27 16:14:00 -0500

    Main structure done

--- CBC-Analyzer/src/main/java/org/example/Main.java
+++ CBC-Analyzer/src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+
+import java.util.*;
+
+import static java.util.Map.entry;
+
 public class Main {
-     public static void main(String[] args) {
-         //BloodParameter is a class I made that holds 4 values: Name, Low end of reference range, high end of reference range, and unit.
-         BloodParameter wbc = new BloodParameter("White Blood Cells", 5, 14.1, "x10^3 / mcL");
+    public static void main(String[] args) {
+        //BloodParameter is a class I made that holds 4 values: Name, Low end of reference range, high end of reference range, and unit.
+        BloodParameter wbc = new BloodParameter("White Blood Cells", 5, 14.1, "x10^3 / mcL");
 
          /*
          Fill in the rest of the parameters into BloodParameter instances like above. They are:
@@ -14,13 +19,27 @@ public class Main {
          - Platelets:                          211 - 621          x10^3 / mcL
           */
 
+        //Create output variables for each parameter. These don't need to be assigned, just declared.
+        String wbcOutput;
 
-         //Create random generator for blood map.
+        //Create random generator for blood map. (Don't do here, do outside main method.
+        randomBloodParameters();
 
-         //Create output variables for each parameter.
+        //Loop through Map and apply conditional logic to fill output variables.
 
-         //Loop through Map and apply conditional logic to fill output variables.
+        //output
+    }
 
-         //output
-     }
+    public static Map<String, Double> randomBloodParameters() {
+        Random rand = new Random();
+
+        //create blood map with parameters for the keys and random double for the values.
+        Map<String, Double> bloodMap = new HashMap<>() {{
+            put("wbc", 0.0);
+            put("Hemoglobin", 0.0);
+        }};
+
+        
+        return bloodMap;
+    }
 }
